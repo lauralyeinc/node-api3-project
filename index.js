00f96a7c4770ef6067bbb170606a29128991aba0
Laura <theimer.laura@gmail.com>
2020-01-21 23:20:33 -0500

    Server running in server.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require("./server.js");
+
+server.listen(3000, () => {
+    console.log('/n********* Magic Happening on localhost:3000 *****');
+}); 
--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2> 👩🏼‍🎤 Let's write some middleware!</h2>`);
 });
 
 //custom middleware
+server.use(express.json());
 
 function logger(req, res, next) {}
 
